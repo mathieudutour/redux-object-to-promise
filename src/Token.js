6fb1a0feba5893cfa310937242c1134107544737
Mathieu Dutour <mathieu@dutour.me>
2016-04-26 15:24:54 +0100

    fix typo

--- src/Token.js
+++ src/Token.js
@@ -1,5 +1,10 @@
 export default ({storage = window.localStorage, key = 'token-key'}) => {
-  let token = storage.getItem(key)
+  let token
+  if (typeof storage.getItem(key) === 'string') {
+    token = storage.getItem(key)
+  } else {
+    storage.getItem(key).then((res) => { token = res })
+  }
   return {
     set (t) {
       token = t
